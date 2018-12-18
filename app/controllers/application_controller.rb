0bcfffd543d078f4a7f2c94ae8b792f7c0e9229d
Courtney <chalt0319@gmail.com>
2018-12-17 21:38:12 -0500

    add descriptive comments to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
     render "layouts/home"
   end
 
-  def check_current_user
+  def check_current_user  # checks to see if the user is the currently logged in user
     if @account
       if @account.id == session[:user_id]
         true
@@ -24,7 +24,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def logged_in?
+  def logged_in?  # checks to see if anyone is logged in
     if !!session[:user_id]
       true
     else
@@ -32,7 +32,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def current_user
+  def current_user  # displays the users name in the navbar
     if logged_in?
       if @account = Account.find_by(id: session[:user_id])
         "#{@account.name}'s Profile"
@@ -44,7 +44,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def current_user_path
+  def current_user_path  # link to the current users home page
     if !!Account.find_by(id: session[:user_id])
       @account = Account.find(session[:user_id])
       account_path(@account)
